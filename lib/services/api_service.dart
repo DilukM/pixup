e1f98cb672d1d99e5617d9fa86b0faae17977a20
Diluk Mihiranga <dilukedu@gmail.com>
2025-01-12 18:19:22 +0530

    add ApiService to fetch movies from The Movie Database API and update dependencies

---
+++ lib/services/api_service.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:pixup/models/movieModel.dart';
+
+class ApiService {
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String apiKey = 'ad6fd113e364ff58fbfd2c8e80545f84';
+  static const String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
+
+  Future<List<Movie>> getAllMovies() async {
+    try {
+      final response = await http.get(
+        Uri.parse('$baseUrl/discover/movie?api_key=$apiKey'),
+      );
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final results = data['results'] as List;
+        return results.map((movie) => Movie.fromJson(movie)).toList();
+      } else {
+        throw Exception('Failed to fetch movies');
+      }
+    } catch (e) {
+      throw Exception('Error fetching movies: $e');
+    }
+  }
+}
